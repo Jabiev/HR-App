3b12232f6adbafabd89b8b964caed7a64dc8a1db
DESKTOP-1424DPE\HP <jabieviam@gmail.com>
2024-07-28 02:44:03 +0400

    established position-service

---
+++ HRproject/HRproject.Business/Implementations/PositionService.cs
@@ -0,0 +1,47 @@
+﻿using HRproject.Business.Exceptions;
+using HRproject.Business.Interfaces;
+using HRproject.Core.Entities;
+
+namespace HRproject.Business.Implementations;
+
+public class PositionService : IPositionService
+{
+    List<Position>? _positions;
+    public void Add(string? name)
+    {
+        if (string.IsNullOrEmpty(name))
+            throw new ValueNullorEmptyException("Invalid Value");
+        var checkname = _positions?.Find(d => d.Name == name);
+        if (checkname is not null)
+            throw new ValueMessException("Already Exists the Value");
+        Position position = new(name);
+        _positions?.Add(position);
+    }
+
+    public void Change(int id, string? name)
+    {
+        if (string.IsNullOrEmpty(name))
+            throw new ValueNullorEmptyException("Invalid value");
+        var position = _positions?.Find(d => d.Id == id);
+        if (position is null)
+            throw new NotFoundException("Not Found Value");
+        var checkname = _positions?.Find(d => d.Name == name);
+        if (checkname is not null)
+            throw new ValueMessException("Already Exists the Value");
+        if (position.Name != name)
+            throw new InconsistencyException("Inconsistent circumstance");
+        position.Name = name;
+    }
+
+    public void Delete(int id)
+    {
+        var position = _positions?.Find(d => d.Id == id);
+        if (position is null)
+            throw new NotFoundException("Not Found Value");
+        //If any employees consist in that so =>
+        _positions?.Remove(position);
+    }
+
+    public List<Position> Info() =>
+        _positions ?? new List<Position>();
+}
---
+++ HRproject/HRproject.Business/Interfaces/IPositionService.cs
@@ -0,0 +1,11 @@
+﻿using HRproject.Core.Entities;
+
+namespace HRproject.Business.Interfaces;
+
+public interface IPositionService
+{
+    void Add(string? name);
+    void Change(int id, string? name);
+    void Delete(int id);
+    List<Position> Info();
+}
